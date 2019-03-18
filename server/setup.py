356a0fae76a1be1a424ac298a6c7315ae7ef612b
MancunianSam <sam.palmer2@protonmail.com>
2019-03-18 11:43:22 +0000

    Add code coverage for python tests

--- server/setup.py
+++ server/setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['recipes'],
     include_package_data=True,
     install_requires=[
-        'flask', 'waitress', 'flask-sqlalchemy', 'flask-cors'
+        'flask', 'waitress', 'flask-sqlalchemy', 'flask-cors', 'coverage'
     ],
 )
